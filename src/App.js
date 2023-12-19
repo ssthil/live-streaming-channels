8a01d2153ab3f318ec614b8b4fb4e13b4a4cc359
Senthil Sivaramakrishnan <ssthil@gmail.com>
2023-12-19 11:32:44 +0530

    fix: url update

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
         "https://indiatoday-pdelivery.akamaized.net/indiatoday/video/2023_10/tooltips/10_oct_23_it_newstrack_pse_fullclip_1010_vt_nb_1024_512-preview.mp4"
       ); */
       setUrl(
-        "https://103.lfjustfor.xyz:8088/stream/lizata1/playlist.m3u8?vidictid=19663594820&id=43662&pk=be861b5b9a5a6cd40f1da003f7fe4d944404db6135722ad9abd4498207347ba2"
+        "https://yupplivegcp1.yuppcdn.net/140223/smil:ddindiahd.smil/chunklist_b1628000.m3u8?hdntl=Expires=1703051606~_GO=Generated~acl=!*/140223/smil:ddindiahd.smil/*!yuppTVCom_5_-1_b1245ce5331d40ce_IN_157.46.86.153/payload/yuppTVCom_5_-1_b1245ce5331d40ce_IN_157.46.86.153/*~Signature=AayxKH5Nd1OWHNM8tVvDpter7nrWOGGm_t1Eyj5HxpkbspRP_rHk_djjZ1NM-G67NBrp3kgLuC3Bz8N3OQ3JsFca_scA"
       );
       // setUrl(
       //   "https://103.lfjustfor.xyz:8088/stream/lizata1/playlist.m3u8?vidictid=196413659337&id=43662&pk=f4a7efb169cc3e35ebc5ac271a9874b1bd0f60bb129740660f59c79bd2771f55"
@@ -28,8 +28,8 @@ function App() {
 
   const channels = [
     {
-      name: "CRICKET",
-      url: "https://103.lfjustfor.xyz:8088/stream/lizata1/playlist.m3u8?vidictid=19663594820&id=43662&pk=be861b5b9a5a6cd40f1da003f7fe4d944404db6135722ad9abd4498207347ba2",
+      name: "DD India",
+      url: "https://yupplivegcp1.yuppcdn.net/140223/smil:ddindiahd.smil/chunklist_b1628000.m3u8?hdntl=Expires=1703051606~_GO=Generated~acl=!*/140223/smil:ddindiahd.smil/*!yuppTVCom_5_-1_b1245ce5331d40ce_IN_157.46.86.153/payload/yuppTVCom_5_-1_b1245ce5331d40ce_IN_157.46.86.153/*~Signature=AayxKH5Nd1OWHNM8tVvDpter7nrWOGGm_t1Eyj5HxpkbspRP_rHk_djjZ1NM-G67NBrp3kgLuC3Bz8N3OQ3JsFca_scA",
     },
     {
       name: "REPUBLIC",
